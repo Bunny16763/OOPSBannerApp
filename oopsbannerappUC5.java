ff2f046e9d2813b8f2eb47338b844e0c3f0f0c57
Bunny Pebbili <sp8071@srmist.edu.in>
2026-03-10 12:43:03 +0530

    oopsbannerappUC5

---
+++ oopsbannerappUC5.java
@@ -0,0 +1,23 @@
+package OOPSBannerApp;
+
+public class oopsbannerappUC5 {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+
+            String.join("  ", "  ****  ", " **** ", " ***     ", "  ******  "),
+            String.join("  ", "*     *", "*      *", "*    **  ", "**        "),
+            String.join("  ", "*     *", "*      *", "*    **  ", "**        "),
+            String.join("  ", "*     *", "*      *", "* **     ", "    ***   "),
+            String.join("  ", "*     *", "*      *", "*        ", "       ** "),
+            String.join("  ", "*     *", "*      *", "*        ", "       ** "),
+            String.join("  ", "  ****  ", "  ****  ", "*        ", "  ****    ")
+
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
